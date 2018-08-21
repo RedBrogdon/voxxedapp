b679ba086bbcfe7414acb383dd4228ad100ae332
Andrew Brogdon <brogdon@gmail.com>
2018-08-21 08:35:11 -0700

    Comments on Rebloc

--- lib/rebloc.dart
+++ lib/rebloc.dart
@@ -23,40 +23,44 @@ import 'package:rxdart/rxdart.dart' show BehaviorSubject;
 /// blocs.
 ///
 /// Here, [Bloc] is used to mean a business logic class that accepts input and
-/// creates output solely through streams. They are given two chances to act in
-/// response to incoming actions: first as middleware, and again as a reducer.
+/// creates output solely through streams. Each [Bloc] is given two chances to
+/// act in response to incoming actions: first as middleware, and again as a
+/// reducer.
 ///
 /// Rather than using functional programming techniques to combine reducers and
-/// middleware, rebloc uses a stream-based approach. A rebloc [Store] creates
+/// middleware, Rebloc uses a stream-based approach. A Rebloc [Store] creates
 /// a dispatch stream for receiving new [Action]s, and invites [Bloc]s to
-/// subscribe and manipulate the stream to perform middleware and reducer
+/// subscribe and manipulate the stream to apply their middleware and reducer
 /// functionality. Where in Redux, one middleware function is responsible for
 /// calling the next, with Rebloc a middleware function receives an action from
 /// its input stream and (after logging, kicking off async APIs, dispatching new
 /// actions, etc.) is responsible for emitting it on its output stream so the
-/// next [Bloc] will have a chance to act on it.
+/// next [Bloc] will have a chance to act. If the middleware function needs to
+/// cancel the action, of course, it can just emit nothing.
 ///
 /// After middleware processing is complete, the Stream of [Action]s is mapped
 /// to one of [Accumulator]s, and each [Bloc] is given a chance to apply reducer
 /// functionality in a similar manner. When finished, the resulting app state is
-/// emitted on the [Store]'s `states` stream, where changes can be picked up by
+/// added to the [Store]'s `states` stream, and changes can be picked up by
 /// [ViewModelSubscriber] widgets.
 ///
-/// [ViewModelSubscriber] is similar to [StreamBuilder] in that it listens to a
-/// stream and builds widgets in response, but with a few key differences:
+/// [ViewModelSubscriber] is intended to be the sole mechanism by which widgets
+/// are built from the data emitted by the [Store] and wired up to dispatch
+/// [Action]s to it. [ViewModelSubscriber] is similar to [StreamBuilder] in that
+/// it listens to a stream and builds widgets in response, but with a few key
+/// differences:
 ///
 /// - It looks for a [StoreProvider] ancestor in order to get a reference to a
 ///   [Store] of matching type.
-/// - It assumes the stream will always emit a value immediately on
-///   subscription (an RxDart BehaviorSubject is used by [Store] to ensure
-///   this). As a result, it has no mechanism for connection states or snapshots
-///   that don't contain data.
+/// - It assumes the stream will always have a value on subscription (an RxDart
+///   BehaviorSubject is used by [Store] to ensure this). As a result, it has no
+///   mechanism for connection states or snapshots that don't contain data.
 /// - It converts the app state objects it receives into view models using a
-///   required `convert` function, and ignores any changes to the app state that
-///   don't cause a change in its view model, limiting rebuilds.
+///   required `converter` function, and ignores any changes to the app state
+///   that don't cause a change in its view model, limiting rebuilds.
 /// - It provides to its builder method not only the most recent view model, but
 ///   also a reference to the [Store]'s `dispatcher` method, so new [Action]s
-///   can be dispatched in response to user actions.
+///   can be dispatched in response to user events like button presses.
 
 /// A Redux-style action. Apps change their overall state by dispatching actions
 /// to the [Store].
@@ -73,10 +77,9 @@ typedef void DispatchFunction(Action action);
 /// functionality in response to incoming [Action]s by subscribing to the
 /// "reducer" stream, which is of type `<Stream<Accumulator<S>>`.
 ///
-/// A [Bloc] that does so is expected use the
-/// [Action] and [state] provided in any [Accumulator] it receives to calculate
-/// a new [state], then emit it in a new Accumulator with the original action
-/// and new [state].
+/// A [Bloc] that does so is expected use the [Action] and [state] provided in
+/// any [Accumulator] it receives to calculate a new [state], then emit it in a
+/// new Accumulator with the original action and new [state].
 class Accumulator<S> {
   final Action action;
   final S state;
@@ -107,9 +110,10 @@ class MiddlewareContext<S> {
 /// A store for app state that manages the dispatch of incoming actions and
 /// controls the stream of state objects emitted in response.
 ///
-/// A [Store] performs a few main jobs:
+/// [Store] performs these tasks:
 ///
-/// - Create the dispatch/reduce stream using an [initialState] value.
+/// - Create a controller for the dispatch/reduce stream using an [initialState]
+///   value.
 /// - Wire each [Bloc] into the dispatch/reduce stream by calling its
 ///   [applyMiddleware] and [applyReducers] methods.
 /// - Expose the [dispatcher] by which a new [Action] can be dispatched.
@@ -266,6 +270,7 @@ class _ViewModelStreamBuilderState<S, V extends ViewModel<S>>
   V _latestViewModel;
   StreamSubscription<V> _subscription;
 
+  // TODO(redbrogdon): make sure this isn't somehow building twice.
   @override
   void initState() {
     super.initState();
